8a09c5ca3932a46339bead5f78ee98dfb258257d
waderaanmol <74456502+waderaanmol@users.noreply.github.com>
2024-07-09 13:26:24 -0400

    Update CreateReporting_Anmol.sql
    Added description

--- GRI Implementation Artifacts/Stored Procedures/Crud/Reporting/CreateReporting_Anmol.sql
+++ GRI Implementation Artifacts/Stored Procedures/Crud/Reporting/CreateReporting_Anmol.sql
@@ -1,5 +1,5 @@
 -- Stored Procedure To Create Reporting Company
--- Takes the two paramters name and status (0 or 1) and inserts new record in reporting table
+-- Takes the two paramters name and status (0 or 1) and inserts new record in the reporting table
 CREATE PROCEDURE [mod].CreateReporting
     @name VARCHAR(255), --Parameter #1
     @status BIT --Parameter #2
